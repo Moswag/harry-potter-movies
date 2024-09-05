dcc9298e27adb0317c0e297e50cced776cb2a2de
Webster Moswa <webroy94@gmail.com>
2024-09-05 17:14:38 +0200

    added movie list view

--- src/app/movie/list/movie-list.component.ts
+++ src/app/movie/list/movie-list.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "../movie.model";
+import {MovieService} from "../service/movie.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+  allMovies: Movie[] = [];
 
+
+  constructor(private movieService: MovieService) {
+  }
+
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe(data => {
+      this.allMovies = data;
+    });
+  }
 }
