7c21ed084e12feb1f084d33313bb53900f59f938
Webster Moswa <webroy94@gmail.com>
2024-09-05 17:09:42 +0200

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {MovieDetailComponent} from "./movie/detail/movie-detail.component";
+import {MovieListComponent} from "./movie/list/movie-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    component: MovieListComponent,
+    data: {
+      title: 'Movie List'
+    },
+  },
+  {
+    path: 'movies/:movieId',
+    component: MovieDetailComponent,
+    data: {
+      title: 'Movie Details'
+    },
+  }
+];
