df1a2b88aed16c51c6a67b3fc2e9cd62e2855c97
Webster Moswa <webroy94@gmail.com>
2024-09-05 17:05:01 +0200

    implemented movies endpoint

--- src/app/movie/service/movie.service.ts
+++ src/app/movie/service/movie.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Movie} from "../movie.model";
+import {MovieDetail} from "../movie-detail.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  protected resourceUrl = '/movies';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.resourceUrl);
+  }
+
+  findMovieById(id: string): Observable<MovieDetail> {
+    return this.http.get<MovieDetail>(`${this.resourceUrl}/${id}`);
+  }
 }
